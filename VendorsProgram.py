f7c73bd1627aa86c170908974976732133f6b4d9
BrandonS16475 <S16475@qmc.ac.uk>
2026-06-16 15:27:56 +0100

    Version 1

---
+++ VendorsProgram.py
@@ -0,0 +1,10 @@
+f = open(r"C:\Users\s16475\Documents\GitHub\HotdogVendors\Hotdogs.txt", "r") # opens the file (used the whole directory because was giving an error)
+print("Welcome to the Hotdog Vendors program.")
+do = input("What do you want to do? Edit(e) Search(s) View(v)")
+if do is "v":
+    for i in f:
+        contents = f.readline() # Reads a singular line from the file. The for loop will allow the whole file to be printed
+        l = contents.split(",")
+        print(l)
+
+f.close()
